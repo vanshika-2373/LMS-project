040e3524ea086d4250d0efc633b2e48f1adf9d2c
vanshika yadav <166695290+vanshika-2373@users.noreply.github.com>
2025-11-23 01:17:14 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://lms-project-ss4c.onrender.com",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
